def839812fe11fbc39392d77c46495601f51e630
guowc <guowc@wopuwulian.com>
2021-10-21 11:12:45 +0800

    2Sum增加双指针解法

--- dawn-parent/dawn-algorithm/src/main/java/org/ycx/lesson/leetcode/array/SumOfTwoNumbers.java
+++ dawn-parent/dawn-algorithm/src/main/java/org/ycx/lesson/leetcode/array/SumOfTwoNumbers.java
@@ -1,18 +1,25 @@
-package org.ycx.lesson.leetcode.easy;
+package org.ycx.lesson.leetcode.array;
 
 
-import java.util.ArrayList;
+import sun.security.util.Length;
+
 import java.util.Arrays;
 import java.util.HashMap;
 
 /**
  * 0001. 两数之和
- * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
- * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ *
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific
+ * target.
+ * You may assume that each input would have exactly one solution, and you may not use the same
+ * element twice
+ *
+ * 题目大意： 在数组中找到 2 个数之和等于给定值的数字，结果返回 2 个数字在数组中的下标。
+ *
  * 示例:
- * 给定 nums = [2, 7, 11, 15], target = 9
- * 因为 nums[0] + nums[1] = 2 + 7 = 9
- * 所以返回 [0, 1]
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1]
  *
  * @author :     guowc
  * @version :    1.0
@@ -45,11 +52,17 @@ public class SumOfTwoNumbers {
         String result2 = resultArray2.length > 0 ? Arrays.toString(resultArray) : "Sorry no result!!!";
         System.out.println(result2);
         System.out.println("方法2消耗时间：" + (System.currentTimeMillis() - st) + "ms");
+
+
+        st = System.currentTimeMillis();
+        Integer[] resultArray3 = sumOfTwoNumbers3(nums, target);
+        String result3 = resultArray3.length > 0 ? Arrays.toString(resultArray) : "Sorry no result!!!";
+        System.out.println(result3);
+        System.out.println("方法2消耗时间：" + (System.currentTimeMillis() - st) + "ms");
     }
 
     /**
-     * 解法1
-     * 暴力循环
+     * 解法1 暴力循环
      * 时间复杂度T(n) = O(f(n²))  空间复杂度为 O(n)
      *
      * @param nums
@@ -76,10 +89,9 @@ public class SumOfTwoNumbers {
 
 
     /**
-     * 解法2
-     * 用map解决 (参考他人思路,非独自思考)
+     * 解法2 用map解决 (参考他人思路,非独自思考)
      * key:target-num[i]  value:数组索引
-     * 时间复杂度T(n) = O(f(n²))  空间复杂度为 O(n)
+     * 时间复杂度T(n) = O(f(n))  空间复杂度为 O(n)
      *
      * @param nums
      * @param target
@@ -100,5 +112,29 @@ public class SumOfTwoNumbers {
         return new Integer[2];
     }
 
-
+    /**
+     * 解法三 ：双指针
+     * 首先将数组排序,计算nums[low]+nums[high] 如果小于target 代表左边的数太小，指针右移，如果大于target代表右边的数太大指针左移
+     * 时间复杂度T(n) = O(f(n))  空间复杂度为 O(n)
+     * @author : guowc
+     * @date   : 2021/10/20 18:10
+     * @param    nums
+     * @param    target
+     * @return : Integer
+     **/
+    public static Integer[] sumOfTwoNumbers3(Integer[] nums, Integer target){
+        int low = 0;
+        int high = nums.length-1;
+        Arrays.sort(nums);
+        while (low < high) {
+            if (nums[low]+nums[high]<target) {
+                low++;
+            }else if (nums[low]+nums[high]>target){
+                high--;
+            }else {
+                return new Integer[]{low,high};
+            }
+        }
+        return new Integer[2];
+    }
 }
