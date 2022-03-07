a5dde190167159b2ab4a3e4b050642043e5e6915
guowc <guowc@wopuwulian.com>
2022-03-07 11:30:57 +0800

    【多线程模块】-模块创建

---
+++ dawn-parent/dawn-multithreading/src/main/java/com/ycx/DawnMultithreadingApplication.java
@@ -0,0 +1,13 @@
+package com.ycx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawnMultithreadingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DawnMultithreadingApplication.class, args);
+    }
+
+}
---
+++ dawn-parent/dawn-multithreading/src/test/java/com/ycx/DawnMultithreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawnMultithreadingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
