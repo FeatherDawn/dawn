d9b390c97c9182cc232bfd4248dd273cc5080f3a
guowc <guowc@wopuwulian.com>
2022-03-31 18:43:27 +0800

    【多线程模块】- 修改守护线程注释

--- dawn-parent/dawn-multithreading/src/main/java/com/ycx/demo/priority/daemon/DaemonThreadDemo.java
+++ dawn-parent/dawn-multithreading/src/main/java/com/ycx/demo/priority/daemon/DaemonThreadDemo.java
@@ -7,16 +7,16 @@ package com.ycx.demo.priority.daemon;
  * @date :       2022/3/31 18:06
  * @copyright :  wpwl
  */
-public class DaemonThreadDemo {    public static void main(String[] args){
+public class DaemonThreadDemo {
 
-        Thread t1 = new CommonThread();
-        Thread t2 = new Thread(new MyDaemonThread());
-        //抛出异常 : IllegalThreadStateException【设置守护线程必须在执行线程之前】
-        //t2.start();
-        //设置守护线程
-        t2.setDaemon(true);
-        //正确做法线程运行前设置守护线程
-        t2.start();
-        t1.start();
+        public static void main(String[] args){
+            Thread t1 = new CommonThread();
+            Thread t2 = new Thread(new MyDaemonThread());
+            //守护线程的设置必须要在执行线程之前，如果在执行线程之后设置守护线程，线程会失效并抛出IllegalThreadStateException异常
+            //t2.start();
+            t2.setDaemon(true);
+            //正确做法在线程执行之前设置守护线程
+            t2.start();
+            t1.start();
     }
 }
